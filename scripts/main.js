40f1294e35538b76c4bd38e14d6aa284e8da273f
Flashspeed <flashspeed@hotmail.co.uk>
2018-02-05 19:44:40 +0000

    Did some stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,44 @@
 let scope = {};
 
-((scope, $) => {
+((scope, $) =>
+{
     //let scope.smth will create "public" variable accessible by scope.smth
     //let smth will create local variable not accessible in console
-    const GAME = {
+    const GAME             = {
         pot: 0,
         cardsOnTable: [],
         numberOfPlayers: null,
-        players: []
+        players: [],
+        currentPlayer: null
     };
+    let currentPlayerIndex = 0;
     let deck;
 
     deck = new Deck();
     deck.shuffleDeck();
 
-    for(let i=0; i<5; i++)
+    for (let i = 0; i < 5; i++)
     {
         let cardDrawn = deck.drawCard();
         cardDrawn.addToTable();
         GAME.cardsOnTable.push(cardDrawn);
     }
 
-    // const playerCard_1 = $(#)
-
     // Raise amount input and slider
-    const raiseInput = document.getElementById('raiseInput');
-    const raiseSlider = document.getElementById('raiseSlider');
-    raiseSlider.oninput = () => {
+    const raiseInput    = document.getElementById('raiseInput');
+    const raiseSlider   = document.getElementById('raiseSlider');
+    raiseSlider.oninput = () =>
+    {
         raiseInput.value = raiseSlider.value;
     }
 
-    raiseInput.onchange = () => {
+    raiseInput.onchange = () =>
+    {
         /* magic that should not be used in real thingy, but i like it c:
-        sets max value to 100 and min to 1 */
-        raiseInput.value = Math.min(Math.max(raiseInput.value, 1), 100);
+         sets max value to 100 and min to 1 */
+        raiseInput.value  = Math.min(Math.max(raiseInput.value, 1), 100);
         raiseSlider.value = raiseInput.value;
     }
-
-    $('#raiseButton').click(()=>{
-        $('#pot').html(parseInt($('#pot').html()) + parseInt(raiseInput.value));
-    });
 
     // Load modal
     $('#startGameModal').modal({
@@ -49,17 +48,46 @@ let scope = {};
 
     // Button that starts the game
     const startButton = document.getElementById('startButton');
+
     startButton.addEventListener('click', startGame);
+    const foldButton  = $("#foldButton");
+    const raiseButton = $('#raiseButton');
 
-    function startGame() {
+    foldButton.on("click", function ()
+    {
+        const currentPlayer = (++currentPlayerIndex < GAME.players.length) ? currentPlayerIndex : 0;
+        setCurrentPlayer(currentPlayer);
+    });
+
+    raiseButton.click(() =>
+    {
+        $('#pot').html(parseInt($('#pot').html()) + parseInt(raiseInput.value));
+
+        const currentPlayer = (++currentPlayerIndex < GAME.players.length) ? currentPlayerIndex : 0;
+        setCurrentPlayer(currentPlayer);
+
+    });
+
+    function startGame()
+    {
         GAME.numberOfPlayers = document.getElementById('numberOfPlayers').value;
-        const initialStake = document.getElementById('initalStake').value;
+        const initialStake   = document.getElementById('initalStake').value;
 
-        for (var i = 0; i < GAME.numberOfPlayers; i++) {
+        /* Put each player inside an array */
+        for (var i = 0; i < GAME.numberOfPlayers; i++)
+        {
             GAME.players.push(setupPlayer("Bob", initialStake));
         }
 
+        /* Make the first player the current player */
+        setCurrentPlayer(0);
+
         console.log(GAME.players);
+    }
+
+    function setCurrentPlayer(playerIndex)
+    {
+        GAME.currentPlayer = GAME.players[playerIndex];
     }
 
     /**
@@ -87,7 +115,7 @@ let scope = {};
         /* Show the card image for the 2 cards */
         for (let i = 0; i < 2; i++)
         {
-            $("#playerCard" + (i+1)).attr("src", player.cards[i].imagePath);
+            $("#playerCard" + (i + 1)).attr("src", player.cards[i].imagePath);
         }
 
         return player;
