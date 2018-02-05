62d795abca200186b6e8c23a91f9c5c507826a96
Flashspeed <flashspeed@hotmail.co.uk>
2018-02-05 14:27:01 +0000

    Added some basic player functions in the player class.

--- scripts/player.js
+++ scripts/player.js
@@ -1,9 +1,31 @@
-class Player {
+class Player
+{
+    constructor(name, funds)
+    {
+        /* Players name */
+        this.name  = name;
 
-  constructor( ) {
-    // this.name
+        /* The amount of money the player has */
+        this.funds = funds;
 
-  }
+        /* Holds the array of cards a player */
+        this.cards = [];
+    }
 
+    /* Used to assign this player with a set of cards */
+    cardsOnHand(card)
+    {
+        this.cards.push(card);
+    }
+
+    /* Let this player place a bet of a certain amount */
+    placeBet(betAmount)
+    {
+        /* Player cannot bet with more money than he has */
+        if (betAmount <= funds)
+        {
+            this.funds -= betAmount;
+        }
+    }
 
 }
