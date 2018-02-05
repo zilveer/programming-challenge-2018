5d46c187ef01ab0afb23ea148d2724ae0587c863
Godrast <32815741+Godrast@users.noreply.github.com>
2018-02-05 12:42:22 +0000

    removed card after drawning

--- scripts/card.js
+++ scripts/card.js
@@ -1,14 +1,20 @@
 class Card{
     
-    constructor(figure, value, symbol){
+    constructor(figure, value, symbol, image){
         this.figure = figure;
         this.symbol = symbol;
         this.value = value;
+        this.image = image;
         this.visible = true;
     }
 
     flip(){
         this.visible = !this.visible;
+    }
+
+    loadImage(){
+        let tempImage= new Image();
+        tempImage.src = image;
     }
 
 
